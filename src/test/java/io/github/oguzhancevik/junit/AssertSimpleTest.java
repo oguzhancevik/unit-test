9b54dce39e80b2430faae981a3514edea8e750c5
Oguzhan Cevik <cevik.ogsan@gmail.com>
2019-07-05 18:36:37 +0300

    Junit Parameter 2

--- src/test/java/io/github/oguzhancevik/junit/AssertSimpleTest.java
+++ src/test/java/io/github/oguzhancevik/junit/AssertSimpleTest.java
@@ -1,21 +1,21 @@
 package io.github.oguzhancevik.junit;
 
 import io.github.oguzhancevik.unittest.calculator.Calculator;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(JUnitParamsRunner)
+@RunWith(JUnitParamsRunner.class)
 public class AssertSimpleTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
-    @Parameterized.Parameters("10, 20, 30")
-    public void calculateSquareMeter(int width, int height, int totalSquareMeter){
+    @Parameters({"10, 2, 20",  "3, 30, 90"})
+    public void calculateSquareMeter(int width, int height, int totalSquareMeter) throws Exception{
         Assert.assertEquals(totalSquareMeter, calculator.calculateSquareMeter(width, height));
-        org.junit.runner.J
     }
 
 }
