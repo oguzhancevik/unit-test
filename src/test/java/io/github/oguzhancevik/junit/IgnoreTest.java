de37e757ed50ffa0ca7d798eb7780c596501c03e
Oguzhan Cevik <cevik.ogsan@gmail.com>
2019-07-08 12:37:17 +0300

    Ignore Test

---
+++ src/test/java/io/github/oguzhancevik/junit/IgnoreTest.java
@@ -0,0 +1,19 @@
+package io.github.oguzhancevik.junit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class IgnoreTest {
+
+    @Test
+    public void testHello() throws Exception{
+        System.out.println("Hello");
+    }
+
+    @Test
+    @Ignore("this method works other time")
+    public void testHello2() throws Exception{
+        System.out.println("Hello2");
+    }
+
+}
