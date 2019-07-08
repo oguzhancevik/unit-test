faa1b7f8e85ccc96a00eb8e94e275709d14521fd
Oguzhan Cevik <cevik.ogsan@gmail.com>
2019-07-08 12:37:06 +0300

    Suit Test (run all)

---
+++ src/test/java/io/github/oguzhancevik/junit/SuitTest.java
@@ -0,0 +1,14 @@
+package io.github.oguzhancevik.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+                {
+        AssertTest.class,
+        ParameterTest.class,
+        LifeCycle.class}
+)
+public class SuitTest {
+}
